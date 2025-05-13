aa7b9408ace843b1a494a7aa1e9bff6638787ee0
gbondar <gonza.bondar@gmail.com>
2025-05-13 15:33:57 -0300

    Agrego cambios en modal mail

--- javascript.js
+++ javascript.js
@@ -69,6 +69,51 @@ window.addEventListener("load", () => {
           });
         });
       });
+
+        const mailBtn = document.getElementById("mail-button");
+        const modal = document.getElementById("mail-modal");
+        const closeModal = document.getElementById("close-modal");
+
+        mailBtn.addEventListener("click", () => {
+        modal.style.display = "flex";
+        });
+
+        // Cerrar el modal
+        closeModal.addEventListener("click", () => {
+        modal.style.display = "none";
+        });
+
+        // Gmail
+        document.getElementById("gmail-btn").addEventListener("click", () => {
+        window.open("https://mail.google.com/mail/?view=cm&to=gonza.bondar@gmail.com", "_blank");
+        });
+
+        // Outlook
+        document.getElementById("outlook-btn").addEventListener("click", () => {
+        window.open("https://outlook.live.com/mail/0/deeplink/compose?to=gonza.bondar@gmail.com", "_blank");
+        });
+
+        // Copiar email
+        document.getElementById("copy-btn").addEventListener("click", () => {
+            const email = "gonza.bondar@gmail.com";
+            navigator.clipboard.writeText(email).then(() => {
+              const msg = document.getElementById("copy-message");
+              msg.style.opacity = "1";
+              msg.textContent = "Dirección de mail copiada al portapapeles";
+              setTimeout(() => {
+                msg.style.opacity = "0";
+              }, 1500);
+            });
+          });
+          
+
+        // Cerrar modal al hacer click fuera
+        window.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+        });
+
       
 
 
